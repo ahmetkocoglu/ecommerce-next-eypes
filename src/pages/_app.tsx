4c33230c89e94d3c0bbf12bec34f5304a783261b
Ahmet Koçoğlu <ahmet.kocoglu@autanate.com>
2024-03-07 21:48:40 +0300

    add basket

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,12 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { store } from "@/store";
+import { ToastContainer } from "react-toastify";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <ToastContainer />
       <Provider store={store}>
         <Component {...pageProps} />
       </Provider>
